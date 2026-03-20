f5a447442e51be9ef772bb5ba983e7db678a8792
KSherman97 <22063898+KSherman97@users.noreply.github.com>
2026-03-20 13:21:56 -0400

    added chunking, threading, config, and logging

--- python/main.py
+++ python/main.py
@@ -10,26 +10,43 @@ Think Apache or NGINX in Python.
 Because my focus is on the workings of HTTP and TCP, there will likely be imperfections
 throughout the code.
 """
-
-import socket as skt
 import os
+import threading
+import socket as skt
 import mimetypes
+import logging
+import json
 
 """
 TODO:
 
-We need to implement chunking (standard is 4kb or 8kb)
-We need to implement logging
-We need to load from a config
-We need to allow default pages to be set for error handling
-We need routing logic, IE. .php serves php pages and everything else is static
-As it stands too many items are hard coded
-We will need to implement some form of multithreading as well
+[x] We need to implement chunking (standard is 4kb or 8kb)
+[x] We will need to implement some form of multithreading as well
+[x] We need to implement logging
+[ ] We need to load from a config
+
+[ ] Investigate using Async IO over threads (for now threads are fine)
+[ ] As it stands too many items are hard coded
+[ ] We need to allow default pages to be set for error handling
+[ ] We need routing logic, IE. .php serves php pages and everything else is static
 """
 
 
 HOST = "127.0.0.1"
 PORT = 8080
+CHUNK_SIZE = 8000
+RESPONSE_404 = b"<h1>404 File Not Found</h1>"
+CONFIG = None
+
+logger = logging.getLogger(__name__)
+logging.basicConfig(
+    filename='output.log',
+    filemode='w', 
+    encoding='utf-8', 
+    level=logging.DEBUG,
+    format='%(asctime)s [%(threadName)s] %(levelname)s: %(message)s'
+    )
+# logger levels(debug, info, warning, error)
 
 """
 process: socket() -> bind() -> listen() -> accept
@@ -37,6 +54,58 @@ process: socket() -> bind() -> listen() -> accept
 Currently a single connection can be made and a response packet will be sent
 """
 
+def load_config():
+    base_path = os.getcwd()
+    config_path = os.path.join(base_path, "config.json")
+
+    defaults = {
+        "HOST": "127.0.0.1",
+        "PORT": 8080,
+        "CHUNK_SIZE": 8192,
+        "WEB_ROOT": "web",
+        "DEFAULT_PAGE": "index.html",
+        "ERROR_404": "404.html"
+    }
+
+    if os.path.exists(config_path):
+        with open(config_path, "r") as config:
+            user_config = json.load(config)
+            defaults.update(user_config)
+        return defaults
+    
+    return defaults
+
+"""
+Run processing the connection pool in its own thread. Each connection is
+assigned its own thread to prevent blocking connections from larger files
+"""
+def handle_client(connection, address):
+    try:
+        logger.info(f"Connected by {address}")
+        connection.settimeout(10)
+        data = connection.recv(1024)
+
+        if data:
+            method, path, headers = parse_request(data)
+            header, body_path = response_builder(path)
+
+            connection.sendall(header)
+            chunk_file(body_path, connection)
+
+            connection.sendall(b'0\r\n\r\n')
+
+    except Exception as e:
+        logger.error(f"Error handling client {address}: {e}")
+    finally:
+        connection.close()
+        logger.info(f"Close connection from {address}")
+
+
+"""
+file_mapper is tasked with mapping a url request path to a physical dir
+on the system. Also fetches the mimetype for the file and returns
+the path and the type
+"""
 def file_mapper(path):
     root_dir = os.getcwd() # get the current working directory
     web_dir = os.path.join(root_dir, "web")
@@ -74,33 +143,54 @@ def parse_request(request):
     Headers
     Blank Line
     Body (page, etc.)
+
+    When we are sending chunks, we will need to also determine the length of each chunk
+    and append it to the front of the header in hex format, but we must also strup the 0x
+    for example: 0x1f40 -> 1f40
 """
 def response_builder(path):
-    file, file_type_tuple = file_mapper(path)
+    file_path, file_type_tuple = file_mapper(path)
     file_type = file_type_tuple[0] if file_type_tuple[0] else "application/octet-stream"
+    status = "200 OK"
 
-    # We need to open the file contents in binary mode due to the nature of HTTP
-    try:
-        with open(file, "rb") as f:
-            file_contents = f.read()
-        status = "200 OK"
-    except FileNotFoundError:
-        file_contents = b"<h1>404 File Not Found</h1>"
-        status = "400 Not Found"
+    if not os.path.exists(file_path):
+        status = "404 Not Found"
         file_type = "text/html"
+        file_path = os.path.join(os.getcwd(), "web", "404.html")
+
+        if not os.path.exists(file_path):
+            # may want to make this more robust in the future
+            pass
 
     # construct the headers
     # use text/html for .html files, but will need to integrate mime-types later
     header = (
         f"HTTP/1.1 {status}\r\n"
         f"Content-Type: {file_type}\r\n"
-        f"Content-Length: {len(file_contents)}\r\n"
+        # f"Content-Length: {len(file_contents)}\r\n"
+        f"Transfer-Encoding: chunked\r\n"
         "Connection: close\r\n"
         "\r\n"
     )
 
     # combine the bytes: header (encoded) + body (already in bytes format)
-    return header.encode("utf-8") + file_contents
+    return header.encode("utf-8"), file_path # file_contents
+
+def chunk_file(full_file, connection):
+    try:
+        with open(full_file, 'rb') as file:
+            while True:
+                chunk = file.read(CHUNK_SIZE)
+                if not chunk:
+                    break
+
+                header_size = f"{len(chunk):x}\r\n".encode('utf-8')
+                # send the chunk
+                connection.sendall(header_size)
+                connection.sendall(chunk + b'\r\n')
+    except Exception as e:
+        logger.error(f"Error reading file: {e}")
+
 
 def main():
     # the socket expects an address family and socket type
@@ -111,23 +201,22 @@ def main():
 
         socket.bind((HOST, PORT))
         socket.listen(5) # allow up to five pooled connections
+        logger.info(f"Server started on {HOST}:{PORT}")
 
         # main client acceptance loop
         # socket.accept is a blocking method as expected
         while True:
             connection, address = socket.accept()
-            with connection:
-                print(f"Connected by {address}")
 
-                # right now this is lazy
-                # we need to implement chunking to prevent overloading 
-                # the socket
-                data = connection.recv(1024)
-                if data:
-                    method, path, headers = parse_request(data)
+            # handle thread creation ot handle each specific connectino
+            client_thread = threading.Thread(
+                target = handle_client,
+                args = (connection, address)
+            )
 
-                                    
-                    connection.sendall(response_builder(path))
+            client_thread.daemon = True
+            client_thread.start()
 
 if __name__ == "__main__":
+    CONFIG = load_config()
     main()
